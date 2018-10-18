fa8bd234d9fc7f115b9e0dfd78ee6b60b8a05a78
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-10-18 14:12:05 +0200

    20181018

---
+++ lang/de.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * Language file Deutsch
+ *
+ * @category  PHP_Blogging
+ * @package   PHP_Easy_Journal
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/easy-journal
+ */
+
+//** Init array
+$ejn_lstr = array();
+
+//** String definitions
+$ejn_lstr['archives']   = "Archive";
+
+$ejn_lstr['by']         = "Angetrieben von";
+
+$ejn_lstr['cont_fail']  = "Fehler beim erstellen des Inhalteordner!";
+
+$ejn_lstr['data']       = "Daten";
+$ejn_lstr['data_fail']  = "Fehler beim erstellen des Datenordner!";
+
+$ejn_lstr['editor']     = "Editor";
+
+$ejn_lstr['get_tip']    = "Klicken Sie hier um eine kostenlose Kopie dieses Skriptes zu erhalten";
+
+$ejn_lstr['head']       = "Titel";
+$ejn_lstr['head_tip']   = "Tippen Sie hier um den Titel de neuen Eintrag einzugeben";
+$ejn_lstr['head_miss']  = "Fehlender Titel!";
+
+$ejn_lstr['new']        = "Neu";
+$ejn_lstr['nothing']    = "Noch keine Einträge.";
+
+$ejn_lstr['old']        = "Alt";
+$ejn_lstr['old_tip']    = "Tippen Sie hier um ältere Einträge zu bearbeiten";
+
+$ejn_lstr['post']       = "Senden";
+$ejn_lstr['post_tip']   = "Klicken Sie hier um den neuen Eintrag zu veröffentlichen";
+
+$ejn_lstr['quit']       = "Beenden";
+$ejn_lstr['quit_tip']   = "Klicken Sie hier um den Editor zu beenden";
+
+$ejn_lstr['rights']     = "Alle Rechte vorbehalten";
+
+$ejn_lstr['text']       = "Text";
+$ejn_lstr['text_tip']   = "Tipen Sie hier um den text des neuen Eintrag einzugeben";
+$ejn_lstr['text_miss']  = "Fehlender Text!";
+
+$ejn_lstr['update']     = "Aktualisieren";
+$ejn_lstr['update_tip'] = "Klicken Sie hier um die veränderte Datendatei zu aktualisieren";
+
+$ejn_lstr['view_tip']   = "Klicken Sie hier um die Datendatei anzuzeigen";
--- lang/en.php
+++ lang/en.php
@@ -1,1 +1,58 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * Language file English
+ *
+ * @category  PHP_Blogging
+ * @package   PHP_Easy_Journal
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/easy-journal
+ */
 
+//** Init array
+$ejn_lstr = array();
+
+//** String definitions
+$ejn_lstr['archives']   = "Archives";
+
+$ejn_lstr['by']         = "Powered by";
+
+$ejn_lstr['cont_fail']  = "Failed to create content folder!";
+
+$ejn_lstr['data']       = "Data";
+$ejn_lstr['data_fail']  = "Failed to create data folder!";
+
+$ejn_lstr['editor']     = "Editor";
+
+$ejn_lstr['get_tip']    = "Click here to get a free copy of this script";
+
+$ejn_lstr['head']       = "Title";
+$ejn_lstr['head_tip']   = "Type here to enter the new entry's title";
+$ejn_lstr['head_miss']  = "Missing title!";
+
+$ejn_lstr['new']        = "New";
+$ejn_lstr['nothing']    = "Nothing here just now.";
+
+$ejn_lstr['old']        = "Old";
+$ejn_lstr['old_tip']    = "Type here to edit old entries";
+
+$ejn_lstr['post']       = "Post";
+$ejn_lstr['post_tip']   = "Click here to post the new entry";
+
+$ejn_lstr['quit']       = "Quit";
+$ejn_lstr['quit_tip']   = "Click here to quit the editor";
+
+$ejn_lstr['rights']     = "All rights reserved";
+
+$ejn_lstr['text']       = "Text";
+$ejn_lstr['text_tip']   = "Type here to enter the new entry's text";
+$ejn_lstr['text_miss']  = "Missing text!";
+
+$ejn_lstr['update']     = "Update";
+$ejn_lstr['update_tip'] = "Click here to update the modified data file";
+
+$ejn_lstr['view_tip']   = "Click here to view data file";
